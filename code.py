e57f657da946cda6c9b046a923d9cf85fa0043ad
Rennn468 <samsonova.ua@phystech.edu>
2024-12-11 01:01:38 +0300

    Загружены изображения и музыка

--- code.py
+++ code.py
@@ -13,6 +13,25 @@ pygame.display.set_caption("Game")
 # Set the frame rate
 clock = pygame.time.Clock()
 
+try:
+    player_image = pygame.image.load("player.png")
+    paw_image = pygame.image.load("paw.png")
+    enemy_image1 = pygame.image.load("enemy1.png")  # Slow enemy
+    enemy_image2 = pygame.image.load("enemy2.png")  # Fast enemy
+    background_image = pygame.image.load("background.png")
+except pygame.error as e:
+    print(f"Error loading images: {e}")
+    pygame.quit()
+    sys.exit()
+
+# load music
+pygame.mixer.music.load('music.mp3')
+# Setting the volume ( 0.0 - 1.0)
+pygame.mixer.music.set_volume(0.5)
+
+# Start music
+pygame.mixer.music.play(-1)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
